48ad2b488bc7388855ccb594a9922997d1baf88f
Łukasz Ordon <lukaszordon21@gmail.com>
2019-08-06 01:00:03 +0200

    First commit
    [WIP] This will output events written in ``plugin.h`` in TS3 console
    To display TS3 console, run it with ``-console`` parameter

---
+++ src/plugin.c
@@ -0,0 +1,257 @@
+/*
+ * TeamSpeak 3 demo plugin
+ *
+ * Copyright (c) 2008-2017 TeamSpeak Systems GmbH
+ */
+
+#ifdef _WIN32
+#pragma warning (disable : 4100)  /* Disable Unreferenced parameter warning */
+#include <Windows.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "teamspeak/public_errors.h"
+#include "teamspeak/public_errors_rare.h"
+#include "teamspeak/public_definitions.h"
+#include "teamspeak/public_rare_definitions.h"
+#include "teamspeak/clientlib_publicdefinitions.h"
+#include "ts3_functions.h"
+#include "plugin.h"
+
+static struct TS3Functions ts3Functions;
+
+#ifdef _WIN32
+#define _strcpy(dest, destSize, src) strcpy_s(dest, destSize, src)
+#define snprintf sprintf_s
+#else
+#define _strcpy(dest, destSize, src) { strncpy(dest, src, destSize-1); (dest)[destSize-1] = '\0'; }
+#endif
+
+#define PLUGIN_API_VERSION 23
+
+#define PATH_BUFSIZE 512
+#define COMMAND_BUFSIZE 128
+#define INFODATA_BUFSIZE 128
+#define SERVERINFO_BUFSIZE 256
+#define CHANNELINFO_BUFSIZE 512
+#define RETURNCODE_BUFSIZE 128
+
+static char* pluginID = NULL;
+
+#ifdef _WIN32
+/* Helper function to convert wchar_T to Utf-8 encoded strings on Windows */
+static int wcharToUtf8(const wchar_t* str, char** result) {
+	int outlen = WideCharToMultiByte(CP_UTF8, 0, str, -1, 0, 0, 0, 0);
+	*result = (char*)malloc(outlen);
+	if(WideCharToMultiByte(CP_UTF8, 0, str, -1, *result, outlen, 0, 0) == 0) {
+		*result = NULL;
+		return -1;
+	}
+	return 0;
+}
+#endif
+
+/*********************************** Required functions ************************************/
+/*
+ * If any of these required functions is not implemented, TS3 will refuse to load the plugin
+ */
+
+/* Unique name identifying this plugin */
+const char* ts3plugin_name() {
+#ifdef _WIN32
+	/* TeamSpeak expects UTF-8 encoded characters. Following demonstrates a possibility how to convert UTF-16 wchar_t into UTF-8. */
+	static char* result = NULL;  /* Static variable so it's allocated only once */
+	if(!result) {
+		const wchar_t* name = L"Aurora GSI Integration";
+		if(wcharToUtf8(name, &result) == -1) {  /* Convert name into UTF-8 encoded result */
+			result = "Aurora GSI Integration";  /* Conversion failed, fallback here */
+		}
+	}
+	return result;
+#else
+	return "Aurora GSI Integration";
+#endif
+}
+
+/* Plugin version */
+const char* ts3plugin_version() {
+    return "0.1";
+}
+
+/* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
+int ts3plugin_apiVersion() {
+	return PLUGIN_API_VERSION;
+}
+
+/* Plugin author */
+const char* ts3plugin_author() {
+	/* If you want to use wchar_t, see ts3plugin_name() on how to use */
+    return "Th3 Ant";
+}
+
+/* Plugin description */
+const char* ts3plugin_description() {
+	/* If you want to use wchar_t, see ts3plugin_name() on how to use */
+    return "Display TeamSpeak3 events as RGB effects in Project Aurora";
+}
+
+/* Set TeamSpeak 3 callback functions */
+void ts3plugin_setFunctionPointers(const struct TS3Functions funcs) {
+    ts3Functions = funcs;
+}
+
+/*
+ * Custom code called right after loading the plugin. Returns 0 on success, 1 on failure.
+ * If the function returns 1 on failure, the plugin will be unloaded again.
+ */
+int ts3plugin_init() {
+    char appPath[PATH_BUFSIZE];
+    char resourcesPath[PATH_BUFSIZE];
+    char configPath[PATH_BUFSIZE];
+	char pluginPath[PATH_BUFSIZE];
+
+    /* Your plugin init code here */
+    printf("PLUGIN: init\n");
+
+    /* Example on how to query application, resources and configuration paths from client */
+    /* Note: Console client returns empty string for app and resources path */
+    ts3Functions.getAppPath(appPath, PATH_BUFSIZE);
+    ts3Functions.getResourcesPath(resourcesPath, PATH_BUFSIZE);
+    ts3Functions.getConfigPath(configPath, PATH_BUFSIZE);
+	ts3Functions.getPluginPath(pluginPath, PATH_BUFSIZE, pluginID);
+
+	printf("PLUGIN: App path: %s\nResources path: %s\nConfig path: %s\nPlugin path: %s\n", appPath, resourcesPath, configPath, pluginPath);
+
+    return 0;  /* 0 = success, 1 = failure, -2 = failure but client will not show a "failed to load" warning */
+	/* -2 is a very special case and should only be used if a plugin displays a dialog (e.g. overlay) asking the user to disable
+	 * the plugin again, avoiding the show another dialog by the client telling the user the plugin failed to load.
+	 * For normal case, if a plugin really failed to load because of an error, the correct return value is 1. */
+}
+
+/* Custom code called right before the plugin is unloaded */
+void ts3plugin_shutdown() {
+    /* Your plugin cleanup code here */
+    printf("PLUGIN: shutdown\n");
+
+	/*
+	 * Note:
+	 * If your plugin implements a settings dialog, it must be closed and deleted here, else the
+	 * TeamSpeak client will most likely crash (DLL removed but dialog from DLL code still open).
+	 */
+
+	/* Free pluginID if we registered it */
+	if(pluginID) {
+		free(pluginID);
+		pluginID = NULL;
+	}
+}
+
+/****************************** Optional functions ********************************/
+/*
+ * Following functions are optional, if not needed you don't need to implement them.
+ */
+
+/* Tell client if plugin offers a configuration window. If this function is not implemented, it's an assumed "does not offer" (PLUGIN_OFFERS_NO_CONFIGURE). */
+int ts3plugin_offersConfigure() {
+	printf("PLUGIN: offersConfigure\n");
+	/*
+	 * Return values:
+	 * PLUGIN_OFFERS_NO_CONFIGURE         - Plugin does not implement ts3plugin_configure
+	 * PLUGIN_OFFERS_CONFIGURE_NEW_THREAD - Plugin does implement ts3plugin_configure and requests to run this function in an own thread
+	 * PLUGIN_OFFERS_CONFIGURE_QT_THREAD  - Plugin does implement ts3plugin_configure and requests to run this function in the Qt GUI thread
+	 */
+	return PLUGIN_OFFERS_NO_CONFIGURE;  /* In this case ts3plugin_configure does not need to be implemented */
+}
+
+/* Plugin might offer a configuration window. If ts3plugin_offersConfigure returns 0, this function does not need to be implemented. */
+void ts3plugin_configure(void* handle, void* qParentWidget) {
+    printf("PLUGIN: configure\n");
+}
+
+/*
+ * If the plugin wants to use error return codes, plugin commands, hotkeys or menu items, it needs to register a command ID. This function will be
+ * automatically called after the plugin was initialized. This function is optional. If you don't use these features, this function can be omitted.
+ * Note the passed pluginID parameter is no longer valid after calling this function, so you must copy it and store it in the plugin.
+ */
+void ts3plugin_registerPluginID(const char* id) {
+	const size_t sz = strlen(id) + 1;
+	pluginID = (char*)malloc(sz * sizeof(char));
+	_strcpy(pluginID, sz, id);  /* The id buffer will invalidate after exiting this function */
+	printf("PLUGIN: registerPluginID: %s\n", pluginID);
+}
+
+/* Plugin command keyword. Return NULL or "" if not used. */
+const char* ts3plugin_commandKeyword() {
+	return "";
+}
+
+void ts3plugin_onConnectStatusChangeEvent(uint64 severConnectionHandlerID, int newStatus, unsigned int errorNumber) {
+
+	if (newStatus == STATUS_CONNECTING) {
+		printf("PLUGIN onConnectStatusChangeEvent: Connecting - status 1");
+	}
+
+	if (newStatus == STATUS_CONNECTED) {
+		printf("PLUGIN onConnectStatusChangeEvent: Connected - status 2");
+	}
+
+	if (newStatus == STATUS_CONNECTION_ESTABLISHING) {
+		printf("PLUGIN onConnectStatusChangeEvent: Establishing connection - status 3");
+	}
+
+	if (newStatus == STATUS_CONNECTION_ESTABLISHED) {
+		printf("PLUGIN onConnectStatusChangeEvent: Established connection - status 4");
+	}
+}
+
+void ts3plugin_onClientMoveEvent(uint64 serverConnectionHandlerID, anyID clientID, uint64 oldChannelID, uint64 newChannelID, int visibility, const char* moveMessage) {
+	
+	printf("PLUGIN onClientMoveEvent: User moved");
+}
+
+void ts3plugin_onClientKickFromChannelEvent(uint64 serverConnectionHandlerID, anyID clientID, uint64 oldChannelID, uint64 newChannelID, int visibility, anyID kickerID, const char* kickerName, const char* kickerUniqueIdentifier, const char* kickMessage) {
+
+	printf("PLUGIN onClientKickFromChannelEvent: User kicked from the channel");
+}
+
+void ts3plugin_onClientKickFromServerEvent(uint64 serverConnectionHandlerID, anyID clientID, uint64 oldChannelID, uint64 newChannelID, int visibility, anyID kickerID, const char* kickerName, const char* kickerUniqueIdentifier, const char* kickMessage) {
+
+	printf("PLUGIN onClientKickFromServerEvent: User kicked from the server");
+}
+
+int ts3plugin_onClientPokeEvent(uint64 serverConnectionHandlerID, anyID fromClientID, const char* pokerName, const char* pokerUniqueIdentity, const char* message, int ffIgnored) {
+    anyID myID;
+
+    printf("PLUGIN onClientPokeEvent: Received PM message");
+
+    return 0;  /* 0 = handle normally, 1 = client will ignore the poke */
+}
+
+int ts3plugin_onTextMessageEvent(uint64 serverConnectionHandlerID, anyID targetMode, anyID toID, anyID fromID, const char* fromName, const char* fromUniqueIdentifier, const char* message, int ffIgnored) {
+
+	printf("PLUGIN: onTextMessageEvent: Received text message");
+
+	return 0;
+}
+
+void ts3plugin_onTalkStatusChangeEvent(uint64 serverConnectionHandlerID, int status, int isReceivedWhisper, anyID clientID) {
+	/* Demonstrate usage of getClientDisplayName */
+	char name[512];
+	if (ts3Functions.getClientDisplayName(serverConnectionHandlerID, clientID, name, 512) == ERROR_ok) {
+		if (status == STATUS_TALKING) {
+			printf("--> %s starts talking\n", name);
+		}
+		else {
+			printf("--> %s stops talking\n", name);
+		}
+	}
+}
+
+void ts3plugin_onHotkeyEvent(const char* keyword) {
+	printf("PLUGIN: Hotkey event: %s\n", keyword);
+	/* Identify the hotkey by keyword ("keyword_1", "keyword_2" or "keyword_3" in this example) and handle here... */
+}
+
