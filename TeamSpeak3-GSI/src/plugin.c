69942703805358a71167065fac4fca40b9ee7a99
SnakePin <18491360+SnakePin@users.noreply.github.com>
2019-08-18 01:55:19 +0300

    Update plugin.c

--- TeamSpeak3-GSI/src/plugin.c
+++ TeamSpeak3-GSI/src/plugin.c
@@ -13,18 +13,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include "teamspeak/public_errors.h"
-#include "teamspeak/public_errors_rare.h"
-#include "teamspeak/public_definitions.h"
-#include "teamspeak/public_rare_definitions.h"
-#include "teamspeak/clientlib_publicdefinitions.h"
-#include "ts3_functions.h"
+
+#include <teamspeak/public_errors.h>
+#include <teamspeak/public_errors_rare.h>
+#include <teamspeak/public_definitions.h>
+#include <teamspeak/public_rare_definitions.h>
+#include <teamspeak/clientlib_publicdefinitions.h>
+#include <ts3_functions.h>
 #include "plugin.h"
 
 #pragma comment(lib, "wldap32.lib" )
 #pragma comment(lib, "crypt32.lib" )
 #pragma comment(lib, "Ws2_32.lib")
- 
+
 #include <curl/curl.h>
 
 static struct TS3Functions ts3Functions;
@@ -52,7 +53,7 @@ static char* pluginID = NULL;
 static int wcharToUtf8(const wchar_t* str, char** result) {
 	int outlen = WideCharToMultiByte(CP_UTF8, 0, str, -1, 0, 0, 0, 0);
 	*result = (char*)malloc(outlen);
-	if(WideCharToMultiByte(CP_UTF8, 0, str, -1, *result, outlen, 0, 0) == 0) {
+	if (WideCharToMultiByte(CP_UTF8, 0, str, -1, *result, outlen, 0, 0) == 0) {
 		*result = NULL;
 		return -1;
 	}
@@ -60,19 +61,14 @@ static int wcharToUtf8(const wchar_t* str, char** result) {
 }
 #endif
 
-/*********************************** Required functions ************************************/
-/*
- * If any of these required functions is not implemented, TS3 will refuse to load the plugin
- */
-
 /* Unique name identifying this plugin */
 const char* ts3plugin_name() {
 #ifdef _WIN32
 	/* TeamSpeak expects UTF-8 encoded characters. Following demonstrates a possibility how to convert UTF-16 wchar_t into UTF-8. */
 	static char* result = NULL;  /* Static variable so it's allocated only once */
-	if(!result) {
+	if (!result) {
 		const wchar_t* name = L"Aurora GSI Integration";
-		if(wcharToUtf8(name, &result) == -1) {  /* Convert name into UTF-8 encoded result */
+		if (wcharToUtf8(name, &result) == -1) {  /* Convert name into UTF-8 encoded result */
 			result = "Aurora GSI Integration";  /* Conversion failed, fallback here */
 		}
 	}
@@ -84,7 +80,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "0.1";
+	return "0.1";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
@@ -95,18 +91,18 @@ int ts3plugin_apiVersion() {
 /* Plugin author */
 const char* ts3plugin_author() {
 	/* If you want to use wchar_t, see ts3plugin_name() on how to use */
-    return "Th3 Ant";
+	return "Th3 Ant 'n SnakePin?";
 }
 
 /* Plugin description */
 const char* ts3plugin_description() {
 	/* If you want to use wchar_t, see ts3plugin_name() on how to use */
-    return "Display TeamSpeak3 events as RGB effects in Project Aurora";
+	return "Display TeamSpeak3 events as RGB effects in Project Aurora";
 }
 
 /* Set TeamSpeak 3 callback functions */
 void ts3plugin_setFunctionPointers(const struct TS3Functions funcs) {
-    ts3Functions = funcs;
+	ts3Functions = funcs;
 }
 
 /*
@@ -114,24 +110,27 @@ void ts3plugin_setFunctionPointers(const struct TS3Functions funcs) {
  * If the function returns 1 on failure, the plugin will be unloaded again.
  */
 int ts3plugin_init() {
-    char appPath[PATH_BUFSIZE];
-    char resourcesPath[PATH_BUFSIZE];
-    char configPath[PATH_BUFSIZE];
+	char appPath[PATH_BUFSIZE];
+	char resourcesPath[PATH_BUFSIZE];
+	char configPath[PATH_BUFSIZE];
 	char pluginPath[PATH_BUFSIZE];
 
-    /* Your plugin init code here */
-    printf("PLUGIN: init\n");
+	/* Your plugin init code here */
+	printf("PLUGIN: init\n");
 
-    /* Example on how to query application, resources and configuration paths from client */
-    /* Note: Console client returns empty string for app and resources path */
-    ts3Functions.getAppPath(appPath, PATH_BUFSIZE);
-    ts3Functions.getResourcesPath(resourcesPath, PATH_BUFSIZE);
-    ts3Functions.getConfigPath(configPath, PATH_BUFSIZE);
+	// Init CURL
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	/* Example on how to query application, resources and configuration paths from client */
+	/* Note: Console client returns empty string for app and resources path */
+	ts3Functions.getAppPath(appPath, PATH_BUFSIZE);
+	ts3Functions.getResourcesPath(resourcesPath, PATH_BUFSIZE);
+	ts3Functions.getConfigPath(configPath, PATH_BUFSIZE);
 	ts3Functions.getPluginPath(pluginPath, PATH_BUFSIZE, pluginID);
 
 	printf("PLUGIN: App path: %s\nResources path: %s\nConfig path: %s\nPlugin path: %s\n", appPath, resourcesPath, configPath, pluginPath);
 
-    return 0;  /* 0 = success, 1 = failure, -2 = failure but client will not show a "failed to load" warning */
+	return 0;  /* 0 = success, 1 = failure, -2 = failure but client will not show a "failed to load" warning */
 	/* -2 is a very special case and should only be used if a plugin displays a dialog (e.g. overlay) asking the user to disable
 	 * the plugin again, avoiding the show another dialog by the client telling the user the plugin failed to load.
 	 * For normal case, if a plugin really failed to load because of an error, the correct return value is 1. */
@@ -139,8 +138,11 @@ int ts3plugin_init() {
 
 /* Custom code called right before the plugin is unloaded */
 void ts3plugin_shutdown() {
-    /* Your plugin cleanup code here */
-    printf("PLUGIN: shutdown\n");
+	/* Your plugin cleanup code here */
+	printf("PLUGIN: shutdown\n");
+
+	// CURL Cleanup
+	curl_global_cleanup();
 
 	/*
 	 * Note:
@@ -148,8 +150,8 @@ void ts3plugin_shutdown() {
 	 * TeamSpeak client will most likely crash (DLL removed but dialog from DLL code still open).
 	 */
 
-	/* Free pluginID if we registered it */
-	if(pluginID) {
+	 /* Free pluginID if we registered it */
+	if (pluginID) {
 		free(pluginID);
 		pluginID = NULL;
 	}
@@ -160,7 +162,7 @@ void ts3plugin_shutdown() {
  * Following functions are optional, if not needed you don't need to implement them.
  */
 
-/* Tell client if plugin offers a configuration window. If this function is not implemented, it's an assumed "does not offer" (PLUGIN_OFFERS_NO_CONFIGURE). */
+ /* Tell client if plugin offers a configuration window. If this function is not implemented, it's an assumed "does not offer" (PLUGIN_OFFERS_NO_CONFIGURE). */
 int ts3plugin_offersConfigure() {
 	printf("PLUGIN: offersConfigure\n");
 	/*
@@ -174,7 +176,7 @@ int ts3plugin_offersConfigure() {
 
 /* Plugin might offer a configuration window. If ts3plugin_offersConfigure returns 0, this function does not need to be implemented. */
 void ts3plugin_configure(void* handle, void* qParentWidget) {
-    printf("PLUGIN: configure\n");
+	printf("PLUGIN: configure\n");
 }
 
 /*
@@ -195,37 +197,30 @@ const char* ts3plugin_commandKeyword() {
 }
 
 int send_state(void) {
-	CURL *curl;
-	CURLcode res;
-
-	/* In windows, this will init the winsock stuff */
-	curl_global_init(CURL_GLOBAL_ALL);
+	CURL* curlHandle;
+	CURLcode curlResult;
 
 	/* get a curl handle */
-	curl = curl_easy_init();
-	if (curl) {
-		/* First set the URL that is about to receive our POST. This URL can
-		   just as well be a https:// URL if that is what should receive the
-		   data. */
-
-		struct curl_slist *headerstruct = NULL;
+	curlHandle = curl_easy_init();
+	if (curlHandle) {
+		struct curl_slist* headerstruct = NULL;
 		headerstruct = curl_slist_append(headerstruct, "Content-Type: application/json");
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerstruct);
-		curl_easy_setopt(curl, CURLOPT_URL, "localhost:9088");
-		/* Now specify the POST data */
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "state=state&key=effect");
+		curl_easy_setopt(curlHandle, CURLOPT_HTTPHEADER, headerstruct);
+		curl_easy_setopt(curlHandle, CURLOPT_URL, "http://localhost:9088");
 
-		/* Perform the request, res will get the return code */
-		res = curl_easy_perform(curl);
-		/* Check for errors */
-		if (res != CURLE_OK)
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-				curl_easy_strerror(res));
+		curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, "insertJSONHere");
 
-		/* always cleanup */
-		curl_easy_cleanup(curl);
+		curlResult = curl_easy_perform(curlHandle);
+
+		// If sending request fails, print the error message
+		if (curlResult != CURLE_OK)
+		{
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(curlResult));
+		}
+
+		curl_easy_cleanup(curlHandle);
 	}
-	curl_global_cleanup();
+
 	return 0;
 }
 
@@ -249,7 +244,7 @@ void ts3plugin_onConnectStatusChangeEvent(uint64 severConnectionHandlerID, int n
 }
 
 void ts3plugin_onClientMoveEvent(uint64 serverConnectionHandlerID, anyID clientID, uint64 oldChannelID, uint64 newChannelID, int visibility, const char* moveMessage) {
-	
+
 	printf("PLUGIN onClientMoveEvent: User moved\n");
 }
 
@@ -264,11 +259,11 @@ void ts3plugin_onClientKickFromServerEvent(uint64 serverConnectionHandlerID, any
 }
 
 int ts3plugin_onClientPokeEvent(uint64 serverConnectionHandlerID, anyID fromClientID, const char* pokerName, const char* pokerUniqueIdentity, const char* message, int ffIgnored) {
-    anyID myID;
+	anyID myID;
 
-    printf("PLUGIN onClientPokeEvent: Received PM message\n");
+	printf("PLUGIN onClientPokeEvent: Received PM message\n");
 
-    return 0;  /* 0 = handle normally, 1 = client will ignore the poke */
+	return 0;  /* 0 = handle normally, 1 = client will ignore the poke */
 }
 
 int ts3plugin_onTextMessageEvent(uint64 serverConnectionHandlerID, anyID targetMode, anyID toID, anyID fromID, const char* fromName, const char* fromUniqueIdentifier, const char* message, int ffIgnored) {
